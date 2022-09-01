31af03a410ed92e4cfa1845d3f9c3e2e2f137042
AkmalBots <112285492+AkmalBots@users.noreply.github.com>
2022-09-02 04:31:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281269734000','6281269734000','6281269734000'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281269734000'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281269734000' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'Akmal' //ubah jadi nama mu, note tanda ' gausah di hapus!
 global.botname = 'Akmal-Botz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.footer = 'Akmal' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
